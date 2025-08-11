0f722a1c36a1401596a3b202d7935dac9ad26557
Abijeet27 <abijeet.sathish@gmail.com>
2025-08-10 23:51:07 -0700

    Update my_home_page.dart

--- my_home_page.dart
+++ my_home_page.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/page1.dart';
 import 'package:flutter_application_1/page2.dart';
-import 'package:flutter_application_1/page3.dart';
 import 'package:flutter_application_1/page5.dart';
-import 'package:flutter_application_1/my_app_state.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -16,37 +14,32 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    List<Widget> options = [Page1(), Page2(), Page3(), Page5()];
+    List<Widget> options = [Page2(), Page5(), Page1()];
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
           body: Center(child: options.elementAt(selectedIndex)),
           bottomNavigationBar:
               BottomNavigationBar(
+                backgroundColor: Color.fromARGB(255, 212, 176, 62),
                 unselectedItemColor: Color.fromARGB(255, 0, 0, 0),
                 selectedItemColor:Color.fromARGB(169, 252, 5, 5), 
                   items: [
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.face_2),
-                      label: "Page 1",
-                    ),
-                    BottomNavigationBarItem(
                       icon: Icon(Icons.face_3),
-                      label: "Page 2",
-                    ),
-                    BottomNavigationBarItem(
-                      icon:Icon(Icons.face),
-                      label: "Page 3",
+                      label: "Registered Clubs",
                     ),
                     BottomNavigationBarItem(
                       icon: Icon(Icons.face_4),
-                      label: "Page 4",
-                    )
+                      label: "Other Clubs",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.settings),
+                      label: "Settings",
+                    ),
                   ],
                   currentIndex: selectedIndex,
                   onTap: (value) {
-                    MyAppState().tile();
-                    MyAppState().tileRegister();
                     setState(() {
                       selectedIndex = value;
                     });
